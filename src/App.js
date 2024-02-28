b3816b761d837e41091c5f0ab5874c9601840225
MUKESHKUSHWAHA41866 <99180742+MUKESHKUSHWAHA41866@users.noreply.github.com>
2024-02-28 06:55:45 +0530

    netflix gpt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+       Namaste Everyone, let's build Netflix Gpt
+    </div>
+  );
+}
+
+export default App;
